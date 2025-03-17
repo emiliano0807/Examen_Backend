5c7df86cba3a7c4235844cf628b0a161eefaeb36
emiliano0807 <emilianorivfacio@gmail.com>
2025-03-17 09:49:57 -0600

    gret.controller

---
+++ controllers/greet.controller.js
@@ -0,0 +1,11 @@
+const {saludar} = require("../service/greet.service")
+
+
+
+const saludarController = (req,res)=>{
+    const mensajeSaludo = saludar()
+    res.json(mensajeSaludo)
+}
+
+
+module.exports = {saludarController}
